75e2cfe2db391c6b47649ba957a1f6ec1a9dfe48
Phinnf <bnm915915@gmail.com>
2025-09-13 11:42:31 +0700

    array test

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Coding_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+
+            ht.Add(10, "apple");
+            Console.WriteLine(ht[10]);
+        }
+    }
+    public class Solution
+    {
+        public bool hasDuplicate(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
