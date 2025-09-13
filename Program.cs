7881d615378da80e7bad2bd369c31141d165f30e
Phinnf <bnm915915@gmail.com>
2025-09-13 12:10:14 +0700

    change the name of the branch

--- Program.cs
+++ Program.cs
@@ -7,25 +7,8 @@ namespace Coding_Practice
     {
         static void Main(string[] args)
         {
-            Hashtable ht = new Hashtable();
-
-            ht.Add(10, "apple");
-            Console.WriteLine(ht[10]);
+           
         }
     }
-    public class Solution
-    {
-        public bool hasDuplicate(int[] nums)
-        {
-            Array.Sort(nums);
-            for (int i = 0; i < nums.Length - 1; i++)
-            {
-                if (nums[i] == nums[i + 1])
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
+    
 }
